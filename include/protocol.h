88c11a0378a228249a509266ec67c760d4ed733e
rrokkam <rohith.rokkam@cs.stonybrook.edu>
2018-07-07 18:35:58 +0000

    Implementation of messaging protocol

--- include/protocol.h
+++ include/protocol.h
@@ -1,16 +1,12 @@
-#ifndef PROTOCOL_NEW_H
-#define PROTOCOL_NEW_H
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
 
 /*
  * Information about the protocol implemented here is available in PROTOCOL.md.
  */
 
 #define MAX_NAME_LEN 16
-#define MAX_MESSAGE_LEN (256 - MAX_NAME_LEN - sizeof keyword_t)
-
-// extern keyword_t keyword_buf;
-// extern char *name_buf;
-// extern char *message_buf;
+#define MAX_MESSAGE_LEN (256 - MAX_NAME_LEN - sizeof(keyword_t))
 
 /* 
  * To add a new message type, add it here and update the utility functions
@@ -33,12 +29,11 @@ void fini();
 
 // bool expect_name(keyword_t keyword);
 // bool expect_message(keyword_t keyword);
-// void send_keyword(int sockfd, keyword_t keyword);
-// void recv_keyword(int sockfd, keyword_t *keyword);
 // void send_with_length(int sockfd, char *buf);
-// void recv_with_length(int sockfd, char **buf);
+// void recv_with_length(int sockfd, char *buf);
 
-// abort the program if we fail a send or received?
+// abort the program if we fail a send or receive?
+// shouldn't send name or message longer than MAX_NAME_LEN or MAX_MESSAGE_LEN respectively.
 void send_message(int sockfd, keyword_t keyword, char *name, char *message);
 void recv_message(int sockfd, keyword_t *keyword, char **name, char **message);
 
--- src/protocol.c
+++ src/protocol.c
@@ -1,18 +1,18 @@
 #include <sys/socket.h>
-#include "protocol_new.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include "protocol.h"
 
 keyword_t keyword_buf;
 char *name_buf;
 char *message_buf;
 
 void init() {
-	keyword_buf = malloc(sizeof keyword_t);
 	name_buf = malloc(MAX_NAME_LEN);
 	message_buf = malloc(MAX_MESSAGE_LEN);
 }
 
 void fini() {
-	free(keyword_buf);
 	free(name_buf);
 	free(message_buf);
 }
@@ -21,6 +21,7 @@ bool expect_name(keyword_t keyword) {
 	switch (keyword) {
 		case LGIN: case EUSR: case SEND: case RECV: case LGOT: return true;
 		case MOTD: case LIST: return false;
+		default: return false;
 	}
 }
 
@@ -28,51 +29,26 @@ bool expect_message(keyword_t keyword) {
 	switch (keyword) {
 		case MOTD: case LIST: case SEND: return true;
 		case LGIN: case EUSR: case RECV: case LGOT: return false;
+		default: return false;
 	}
 }
 
-void send_keyword(int sockfd, keyword_t keyword) {
-//	char *keyword_string = NULL;  // give error on invalid keyword
-	// switch (keyword) {
-	// 	case LGIN: keyword_string = "LGIN"; break;
-	// 	case MOTD: keyword_string = "MOTD"; break;
-	// 	case EUSR: keyword_string = "EUSR"; break;
-	// 	case LIST: keyword_string = "LIST"; break;
-	// 	case SEND: keyword_string = "SEND"; break;
-	// 	case RECV: keyword_string = "RECV"; break;
-	// 	case LGOT: keyword_string = "LGOT"; break;
-	// }	
-	send(sockfd, keyword, sizeof keyword_t, 0);
-}
-
-void recv_keyword(int sockfd, keyword_t *keyword) {
-	keyword = &keyword_buf;
-//	char *keyword_string = NULL;
-	recv(sockfd, keyword, sizeof keyword_t, 0);
-	// if (!strncmp(keyword_string, "LGIN", KEYWORD_STRING_LEN)) {
-	// 	keyword = LGIN;
-	// } else if (!strncmp(keyword_string, "MOTD", KEYWORD_STRING_LEN)) {
-	// } else if (!strncmp(keyword_string, "EUSR", KEYWORD_STRING_LEN)) {
-	// } else if (!strncmp(keyword_string, "LIST", KEYWORD_STRING_LEN)) {
-	// } else if (!strncmp(keyword_string, "SEND", KEYWORD_STRING_LEN)) {
-	// } else if (!strncmp(keyword_string, "RECV", KEYWORD_STRING_LEN)) {
-	// } else if (!strncmp(keyword_string, "LGOT", KEYWORD_STRING_LEN)) {
-	// }
-}
-
 void send_with_length(int sockfd, char *buf) {
-
+	int length = strlen(buf);
+	send(sockfd, &length, sizeof(int), 0);
+	send(sockfd, buf, length, 0);
 }
 
-void recv_with_length(int sockfd, char **buf) {
-
+void recv_with_length(int sockfd, char *buf) {
+	int length;
+	recv(sockfd, &length, sizeof(int), 0);
+	recv(sockfd, buf, length, 0); 
 }
 
-// give keyword, name, message arguments or just use them via the globals
 // need to add handling for when send/recv return an error
 // where to do the length checking for arguments?
 void send_message(int sockfd, keyword_t keyword, char *name, char *message) {
-	send_keyword(sockfd, keyword);
+	send(sockfd, &keyword, sizeof(keyword_t), 0);
 	if (expect_name(keyword)) {
 		send_with_length(sockfd, name);
 	}
@@ -85,7 +61,7 @@ void recv_message(int sockfd, keyword_t *keyword, char **name, char **message) {
 	keyword = &keyword_buf;
 	name = &name_buf;
 	message = &message_buf;
-	recv_keyword(sockfd, keyword_buf);
+	recv(sockfd, keyword, sizeof(keyword_t), 0);
 	if (expect_name(keyword_buf)) {
 		recv_with_length(sockfd, name_buf);
 	}
@@ -93,3 +69,4 @@ void recv_message(int sockfd, keyword_t *keyword, char **name, char **message) {
 		recv_with_length(sockfd, message_buf);
 	}
 }
+
